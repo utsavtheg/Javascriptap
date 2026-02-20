c53a151753622ca2d9c83246cb4d97143c236e4b
Utsav Gupta <utsavgupta309@gmail.com>
2026-02-20 16:59:27 +0530

    Add

---
+++ app.js
@@ -0,0 +1,34 @@
+// Arrays Methods
+// .for each
+// . map
+// . filter
+// .some
+// .every
+// .reduce
+
+/* forEach 
+arr.forEach(some function definition or name)
+eg. forEach
+let arr = [1,2,3,4,5];
+function print(el){
+    console.log(el);
+    }
+    arr.forEach(print);
+    //or
+
+    arr.forEach(function(el){
+    console.log(el);
+});
+
+
+
+*/
+
+console.log("JS loaded");
+
+
+// let arr = [1,2,3,4,5];
+// arr.forEach(function (el){
+//   console.log(el);
+// })
+
